7ea4a9193e35ddc618c2c349bdf47ff2a233d412
Leekangmin <kkk5474096@naver.com>
2022-01-26 01:25:40 +0900

    [feat] ListAdapter 변경

--- app/src/main/java/org/sopt/soptandroidseminar/adapter/RepoListAdapter.kt
+++ app/src/main/java/org/sopt/soptandroidseminar/adapter/RepoListAdapter.kt
@@ -1,22 +1,18 @@
 package org.sopt.soptandroidseminar.adapter
 
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import org.sopt.soptandroidseminar.api.data.response.ResponseRepoInfo
-import org.sopt.soptandroidseminar.api.data.response.ResponseUserInfo
+import org.sopt.soptandroidseminar.api.data.response.ResponseRepo
+import org.sopt.soptandroidseminar.api.data.response.ResponseUser
 import org.sopt.soptandroidseminar.databinding.ItemRepoListBinding
 import org.sopt.soptandroidseminar.util.MyDiffUtil
-import org.sopt.soptandroidseminar.util.MyTouchHelperCallback
 import java.util.*
 
-class RepoListAdapter : RecyclerView.Adapter<RepoListAdapter.RepoViewHolder>(),
-    MyTouchHelperCallback.OnItemMoveListener {
-
-    private val repoList = mutableListOf<ResponseRepoInfo>()
-
-    private lateinit var itemClickListener: ItemClickListener
+class RepoListAdapter : ListAdapter<ResponseRepo, RepoListAdapter.RepoViewHolder>(DIFFUTIL){
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,52 +23,51 @@ class RepoListAdapter : RecyclerView.Adapter<RepoListAdapter.RepoViewHolder>(),
         return RepoViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = repoList.size
-
-    fun setItems(newItems: List<ResponseRepoInfo>) {
-        val diffUtil = MyDiffUtil(repoList, newItems)
-        val diffResult = DiffUtil.calculateDiff(diffUtil)
-
-        repoList.clear()
-        repoList.addAll(newItems)
-        diffResult.dispatchUpdatesTo(this)
-    }
-
-    interface OnStartDragListener {
-        fun onStartDrag(viewHolder: RepoViewHolder)
-    }
-
-    override fun onItemMove(fromPosition: Int, toPosition: Int) {
-        Collections.swap(repoList, fromPosition, toPosition)
-        notifyItemMoved(fromPosition, toPosition)
-    }
-
-    override fun onItemSwipe(position: Int) {
-        repoList.removeAt(position)
-        notifyItemRemoved(position)
-    }
-
-    fun afterDragAndDrop() {
-        notifyDataSetChanged()
-    }
+//    interface OnStartDragListener {
+//        fun onStartDrag(viewHolder: RepoViewHolder)
+//    }
+//
+//    override fun onItemMove(fromPosition: Int, toPosition: Int) {
+//        Collections.swap(repoList, fromPosition, toPosition)
+//        notifyItemMoved(fromPosition, toPosition)
+//    }
+//
+//    override fun onItemSwipe(position: Int) {
+//        repoList.removeAt(position)
+//        notifyItemRemoved(position)
+//    }
+//
+//    fun afterDragAndDrop() {
+//        notifyDataSetChanged()
+//    }
 
     override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
-        holder.onBind(repoList[position])
+        holder.onBind(getItem(position))
     }
 
     class RepoViewHolder(
         private val binding: ItemRepoListBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(gitHubRepoInfo: ResponseRepoInfo) {
+        fun onBind(gitHubRepoInfo: ResponseRepo) {
             binding.repo = gitHubRepoInfo
         }
     }
 
-    interface ItemClickListener {
-        fun onClick(responseUserInfo: ResponseUserInfo)
-    }
+    companion object {
+        val DIFFUTIL = object : DiffUtil.ItemCallback<ResponseRepo>() {
+            override fun areItemsTheSame(
+                oldItem: ResponseRepo,
+                newItem: ResponseRepo
+            ): Boolean {
+                return oldItem.name == newItem.name
+            }
 
-    fun setItemClickListener(itemClickListener: ItemClickListener) {
-        this.itemClickListener = itemClickListener
+            override fun areContentsTheSame(
+                oldItem: ResponseRepo,
+                newItem: ResponseRepo
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 }
