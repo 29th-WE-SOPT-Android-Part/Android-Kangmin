49bedad8a6dfbe0b278aa4a94fcd1e93fac64394
Leekangmin <kkk5474096@naver.com>
2022-01-26 01:26:16 +0900

    [feat] 폴더링

--- app/src/main/java/org/sopt/soptandroidseminar/api/GithubApiService.kt
+++ app/src/main/java/org/sopt/soptandroidseminar/api/GithubApiService.kt
@@ -1,17 +1,17 @@
 package org.sopt.soptandroidseminar.api
 
-import org.sopt.soptandroidseminar.api.data.response.ResponseRepoInfo
-import org.sopt.soptandroidseminar.api.data.response.ResponseUserInfo
+import org.sopt.soptandroidseminar.api.data.response.ResponseRepo
+import org.sopt.soptandroidseminar.api.data.response.ResponseUser
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface GithubApiService {
     @GET("users/kkk5474096/repos")
-    fun reposForUser(): Call<List<ResponseRepoInfo>>
+    fun reposForUser(): Call<List<ResponseRepo>>
 
     @GET("users/kkk5474096")
-    fun getUserInfo(): Call<ResponseUserInfo>
+    fun getUserInfo(): Call<ResponseUser>
 
     @GET("users/kkk5474096/followers")
-    fun getFollowingInfo(): Call<List<ResponseUserInfo>>
+    fun getFollowingInfo(): Call<List<ResponseUser>>
 }
---
+++ app/src/main/java/org/sopt/soptandroidseminar/api/data/response/ResponseRepo.kt
@@ -0,0 +1,7 @@
+package org.sopt.soptandroidseminar.api.data.response
+
+data class ResponseRepo(
+    val name: String,
+    val description: String,
+    val language: String,
+)
