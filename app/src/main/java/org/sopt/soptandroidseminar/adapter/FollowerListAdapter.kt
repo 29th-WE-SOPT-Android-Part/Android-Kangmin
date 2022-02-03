2fb0c447874fa74cb05536749da4ddffbc5c959b
Leekangmin <kkk5474096@naver.com>
2022-02-03 17:40:51 +0900

    [feat] ListAdapter data class 변경

--- app/src/main/java/org/sopt/soptandroidseminar/adapter/FollowerListAdapter.kt
+++ app/src/main/java/org/sopt/soptandroidseminar/adapter/FollowerListAdapter.kt
@@ -5,12 +5,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import org.sopt.soptandroidseminar.data.response.ResponseUser
 import org.sopt.soptandroidseminar.databinding.ItemFollowerListBinding
-import java.util.*
+import org.sopt.soptandroidseminar.domain.entity.GithubFollow
 
 class FollowerListAdapter(private val listener: ItemClickListener) :
-    ListAdapter<ResponseUser, FollowerListAdapter.FollowerUserViewHolder>(DIFFUTIL) {
+    ListAdapter<GithubFollow, FollowerListAdapter.FollowerUserViewHolder>(DIFFUTIL) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -25,14 +24,14 @@ class FollowerListAdapter(private val listener: ItemClickListener) :
     }
 
     fun interface ItemClickListener {
-        fun onClick(data: ResponseUser)
+        fun onClick(data: GithubFollow)
     }
 
     class FollowerUserViewHolder(
         private val binding: ItemFollowerListBinding,
         private val listener: ItemClickListener
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(followerUser: ResponseUser) {
+        fun onBind(followerUser: GithubFollow) {
             binding.user = followerUser
 
             binding.root.setOnClickListener {
@@ -42,17 +41,17 @@ class FollowerListAdapter(private val listener: ItemClickListener) :
     }
 
     companion object {
-        private val DIFFUTIL = object : DiffUtil.ItemCallback<ResponseUser>() {
+        private val DIFFUTIL = object : DiffUtil.ItemCallback<GithubFollow>() {
             override fun areItemsTheSame(
-                oldItem: ResponseUser,
-                newItem: ResponseUser
+                oldItem: GithubFollow,
+                newItem: GithubFollow
             ): Boolean {
                 return oldItem.id == newItem.id
             }
 
             override fun areContentsTheSame(
-                oldItem: ResponseUser,
-                newItem: ResponseUser
+                oldItem: GithubFollow,
+                newItem: GithubFollow
             ): Boolean {
                 return oldItem == newItem
             }
