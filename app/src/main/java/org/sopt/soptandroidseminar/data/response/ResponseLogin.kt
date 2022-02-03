e77f8df88eb742278102048511654b02549f93c6
Leekangmin <kkk5474096@naver.com>
2022-02-03 17:41:56 +0900

    [feat] 확장함수 추가

--- app/src/main/java/org/sopt/soptandroidseminar/data/response/ResponseLogin.kt
+++ app/src/main/java/org/sopt/soptandroidseminar/data/response/ResponseLogin.kt
@@ -1,7 +1,11 @@
 package org.sopt.soptandroidseminar.data.response
 
+import org.sopt.soptandroidseminar.domain.entity.Auth
+
 data class ResponseLogin(
     val email: String,
     val id: Int,
     val name: String
-)
+) {
+    fun toAuth(statusCode: Int) = Auth(email, id, name, statusCode)
+}
