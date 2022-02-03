297747ec54cdd8d64915bcd2099e854730d283c7
Leekangmin <kkk5474096@naver.com>
2022-02-03 17:42:46 +0900

    [feat] ApiService 변경

--- app/src/main/java/org/sopt/soptandroidseminar/data/service/GithubApiService.kt
+++ app/src/main/java/org/sopt/soptandroidseminar/data/service/GithubApiService.kt
@@ -2,16 +2,19 @@ package org.sopt.soptandroidseminar.data.service
 
 import org.sopt.soptandroidseminar.data.response.ResponseRepo
 import org.sopt.soptandroidseminar.data.response.ResponseUser
-import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface GithubApiService {
+    @Headers("Authorization: token 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")
     @GET("users/kkk5474096/repos")
-    fun reposForUser(): Call<List<ResponseRepo>>
+    suspend fun reposForUser(): List<ResponseRepo>
 
+    @Headers("Authorization: token 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")
     @GET("users/kkk5474096")
-    fun getUserInfo(): Call<ResponseUser>
+    suspend fun getUserInfo(): ResponseUser
 
+    @Headers("Authorization: token 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")
     @GET("users/kkk5474096/followers")
-    fun getFollowingInfo(): Call<List<ResponseUser>>
+    suspend fun followingInfo(): List<ResponseUser>
 }
